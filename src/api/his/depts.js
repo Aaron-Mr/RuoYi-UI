1dbe4eb77346478167e9253e6084b668810d5383
Aaron <603562833@qq.com>
2022-08-14 01:42:47 +0800

    RuoYi前端工程

---
+++ src/api/his/depts.js
@@ -0,0 +1,45 @@
+import request from '@/utils/request'
+
+// 查询科室基础(系统管理)列表
+export function listDepts(query) {
+  return request({
+    url: '/his/depts/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询科室基础(系统管理)详细
+export function getDepts(deptsId) {
+  return request({
+    url: '/his/depts/' + deptsId,
+    method: 'get'
+  })
+}
+
+// 新增科室基础(系统管理)
+export function addDepts(data) {
+  return request({
+    url: '/his/depts',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改科室基础(系统管理)
+export function updateDepts(data) {
+  return request({
+    url: '/his/depts',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除科室基础(系统管理)
+export function delDepts(deptsId) {
+  return request({
+    url: '/his/depts/' + deptsId,
+    method: 'delete'
+  })
+}
+
